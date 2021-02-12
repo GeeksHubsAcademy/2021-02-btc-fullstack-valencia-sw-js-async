8b1cf8b3c5b83ee41cb0fad247587eb1c6d613b8
Jose <jmarinmalagon@gmail.com>
2021-02-12 21:18:06 +0100

    api

--- JSasync/get_Api01/js/funtion.js
+++ JSasync/get_Api01/js/funtion.js
@@ -1,64 +1,21 @@
-//compilamos con node.js
 
-var a = 12;
-var b = 13;
+//seteamos valor del div
+const muestra= document.getElementById("data");
 
-console.log(a+b);
+//devuelve todos los campos del JSON 
+fetch('https://api.chucknorris.io/jokes/random')
+ .then(response => response.json())
+ .then(random=>muestra.innerHTML=random.value)
+ .then(random=>console.log(random));
+
+//devuelve todos los campos del JSON 
+fetch('https://api.chucknorris.io/jokes/categories')
+ .then(response => response.json())
+ .then(console.log);
+
+//devuelve el valor de un campo del JSON
+fetch('https://api.chucknorris.io/jokes/categories')
+ .then(response => response.json())
+ .then(categories=>console.log(categories[5]));
 
 
-//alert("Adios!!!!");
-
-
-function getPosts(success, error) {
-    var req = new XMLHttpRequest();
-        req.open('GET', 'https://api.chucknorris.io/jokes/random');
-    
-        req.onload = function() {
-          if (req.status == 200) {
-            success(JSON.parse(req.response));
-          }
-          else {
-            error();
-          }
-        };
-
-        req.send();
-    }
-    /*
-    getPosts(
-        function(r) {
-            console.log(r)
-        },
-        function(r) {
-            console.log('Ocurrió un error')
-        },
-    )*/
-
-    //console.log(r.value);
-
-/*
-    //Promise
-
-    function getPosts() {
-        return new Promise(function(resolve, reject) {
-        var req = new XMLHttpRequest();
-            req.open('GET', 'https://api.chucknorris.io/jokes/random');
-    
-            req.onload = function() {
-              if (req.status == 200) {
-                resolve(JSON.parse(req.response));
-              }
-              else {
-                reject();
-              }
-            };
-    
-            req.send();
-        })
-    }
-    
-    getPosts().then(r =>{
-        console.log(r);
-      }).catch(() => {
-        console.log('Algo salió mal');
-      });*/
